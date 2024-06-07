7ed9be79fc5866ce9fa6b56e7396150ce05b09d0
sandersonsamuel <sandersonsamuel401@gmail.com>
2024-06-07 11:42:00 -0300

    feat: permitindo cors de todas as origens

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,6 @@ import {router} from "./routes";
 
 export const app = express();
 
-app.use(cors({
-    origin: 'https://biblioteca-azevedo-site.vercel.app/',
-}));
+app.use(cors())
 app.use(express.json());
 app.use(router)
